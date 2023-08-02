69efbeaf0128610a6b58bcc699ba90cb4aacffa9
raoathar00786 <raoathar.200@gmail.com>
2023-08-02 19:28:29 +0530

    Python file

---
+++ flipkart_scraper.py
@@ -0,0 +1,66 @@
+import argparse
+import bs4
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+from prettytable import PrettyTable
+import csv
+
+def scrape_flipkart_products(page_url):
+    # opening url and grabbing the web page
+    uClient = urlopen(page_url)
+    page_html = uClient.read()
+    uClient.close()
+
+    # html parsing
+    page_soup = soup(page_html, 'html.parser')
+
+    # grabbing all containers with class name = item-container
+    containers = page_soup.findAll('div', {'class': '_13oc-S'})
+
+    filename = "products.csv"
+
+    headers = ["Product Name", "Price", "Offer", "Specs"]
+
+    with open(filename, 'a', encoding='utf-8', newline='') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(headers)
+
+        product_table = PrettyTable(headers)
+
+        for container in containers:
+            # Extracting product name
+            product_name_container = container.find('div', {'class': '_4rR01T'})
+            product_name = product_name_container.text.strip() if product_name_container else 'N/A'
+
+            # Extracting price
+            price_container = container.find('div', {'class': '_30jeq3 _1_WHN1'})
+            price = price_container.text.strip() if price_container else 'N/A'
+
+            # Extracting specs
+            spec_container = container.find('li', {'class': 'rgWa7D'})
+            specs = spec_container.text.strip() if spec_container else 'N/A'
+
+            # Extracting Offer %
+            Offer_container = container.find('div', {'class': '_3Ay6Sb'})
+            Offer = Offer_container.text.strip() if Offer_container else 'N/A'
+
+            # Writing to the CSV file
+            csv_writer.writerow([product_name.replace(',', '|'), price.replace(',', ''), Offer.replace(',', ' '), specs.replace(',', '|')])
+
+            # Adding to the PrettyTable
+            product_table.add_row([product_name, price, Offer, specs])
+
+        # Print the PrettyTable
+        print(product_table)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Scrape Flipkart mobile products and display in a table.")
+    parser.add_argument('page_number', type=int, help="Page number of the Flipkart page to scrape.")
+    args = parser.parse_args()
+
+    base_url = 'https://www.flipkart.com/search?q=phone&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_2_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_2_na_na_ps&as-pos=1&as-type=RECENT&suggestionId=phone%7CMobiles&requestId=53cf1081-ec8b-4ece-ad30-d2926b2026c9&as-searchtext=ph&page={}'
+    page_number = args.page_number
+    url_to_scrape = base_url.format(page_number)
+
+    scrape_flipkart_products(url_to_scrape)
